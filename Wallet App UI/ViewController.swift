85f122cc30eab4b6eaa7f23e76aa3a0d89f328cd
Yash Shah <yash.j@zomato.com>
2022-03-01 00:12:49 +0530

    Initial Commit

---
+++ Wallet App UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wallet App UI
+//
+//  Created by Yash Shah on 01/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
